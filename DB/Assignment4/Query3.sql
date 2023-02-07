176760e33275f484e446ce5cb7e02f13d215aa69
Umang Gohel <gohelumang12@gmail.com>
2023-02-07 16:06:01 +0530

    Completed SQL Assignment 4

--- DB/Assignment4/Query3.sql
+++
@@ -1,17 +0,0 @@
---3. write a SQL query to Create Stored procedure in the Northwind database to retrieve
---Sales by YearSELECT Year(orderdate) FROM orders;
-SELECT * FROM orders WHERE Year(orderdate)=1996;
-
-
-CREATE PROCEDURE spGetSalesByYear
-@year int
-AS
-BEGIN
-	SELECT * FROM orders WHERE Year(orderdate)=@year;
-END
-
-DROP PROCEDURE spGetSalesByYear;
-
-DECLARE @year int
-SET @year=1996;
-EXECUTE spGetSalesByYear @year;
