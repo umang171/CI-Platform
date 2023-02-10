0b8cdde50b5eb110050ae891c859cad73783bfee
Umang Gohel <gohelumang12@gmail.com>
2023-02-10 17:25:52 +0530

    Create .Net MVC Application for CI Platform

---
+++ MVC/CIPlatform/CIPlatform/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CIPlatform.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult NewPassword()
+        {
+            return View();
+        }
+
+    }
+}
