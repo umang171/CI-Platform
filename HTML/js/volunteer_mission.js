7fe8cdda76b9aedc585f1a2e93d018402005cf2f
Umang Gohel <gohelumang12@gmail.com>
2023-02-22 18:27:17 +0530

    Create Responsive volunteering mission page

--- HTML/js/volunteer_mission.js
+++ HTML/js/volunteer_mission.js
@@ -66,3 +66,32 @@ function myFunction2(y) {
 let y = window.matchMedia("(min-width: 768px)")
 myFunction2(y) // Call listener function at run time
 y.addListener(myFunction2) // Attach listener function on state changes
+
+// tabs in volunteer
+function openCity(evt, cityName) {
+  console.log(evt);
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+let tabcontent = document.getElementsByClassName("tabcontent");
+let tablinks = document.getElementsByClassName("tablinks");
+document.getElementById("Mission").style.display = "block";
+tablinks[0].classList.add("active")
