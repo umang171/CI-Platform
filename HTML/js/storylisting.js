7850b8329d9117c7b85bb247ec69a3f2751f3e7a
Umang Gohel <gohelumang12@gmail.com>
2023-02-23 18:10:59 +0530

    start working on story listing page

---
+++ HTML/js/storylisting.js
@@ -0,0 +1,115 @@
+// Search filter
+const filteMissionNavbar=document.getElementById("filter-mission-navbar");
+const searchButton = document.getElementById("search-button");
+const missionContent=document.getElementById("story-content");
+let missionContentHeight=missionContent.style.height;
+
+console.log(missionContent);
+console.log(missionContentHeight);
+
+function getStyle(element) {
+  if (typeof getComputedStyle !== "undefined") {
+      return getComputedStyle(element);
+  }
+  return element.currentStyle; // Old IE
+}
+
+var heightStyle = getStyle(missionContent).height;
+heightStyle=+heightStyle.slice(0,-2)+78;
+console.log(heightStyle);
+
+
+let flag=0;
+let flagSideBar=0;
+
+searchButton.addEventListener("click",(e)=>{
+  e.preventDefault();
+  if(flag%2==0){
+        filteMissionNavbar.setAttribute('style','display:block !important');
+        missionContent.setAttribute('style','height:'+(heightStyle-79)+'px;');        
+      }
+      else{
+        filteMissionNavbar.setAttribute('style','display:none !important');
+        missionContent.setAttribute('style','height:'+heightStyle+'px;');
+    }
+    flag++;
+});
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    filteMissionNavbar.setAttribute('style','display:block !important');
+    missionContent.setAttribute('style','height:'+(heightStyle-79)+'px;');
+
+    
+  } else {
+    filteMissionNavbar.setAttribute('style','display:none !important');
+    missionContent.setAttribute('style','height:'+heightStyle+'px;');
+
+  }
+}
+
+var x = window.matchMedia("(min-width: 576px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
+// =================================================================================
+// menu-sidebar
+// =================================================================================
+const menuImg=document.getElementById("menuimg");
+const sideBar=document.getElementById("menu-side-bar");
+const closeImg=document.getElementById("close-img");
+const filterSideBar = document.getElementById("filter-side-bar");
+
+menuImg.addEventListener("click",(e)=>{
+  e.preventDefault();
+  if(flagSideBar%2==0){
+    sideBar.setAttribute('style','left:0px !important');
+  }
+flagSideBar++;
+});
+
+closeImg.addEventListener("click",(e)=>{
+  e.preventDefault();
+  if(flagSideBar%2!=0){
+    sideBar.setAttribute('style', 'left:-50vw !important');
+  }
+  flagSideBar++;
+});
+
+function myFunction2(y) {
+  if (y.matches) { // If media query matches
+    sideBar.setAttribute('style', 'display:none !important');
+    filterSideBar.setAttribute('style','display:none !important');    
+  }
+}
+
+let y = window.matchMedia("(min-width: 768px)")
+myFunction2(y) // Call listener function at run time
+y.addListener(myFunction2) // Attach listener function on state changes
+
+// =====================================================================================================
+// filter - button
+// =====================================================================================================
+const filterImg = document.getElementById("filter-image");
+const filtercloseImg = document.getElementById("filter-img-close");
+
+let flagFilter = 0;
+
+filterImg.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (flagFilter % 2 == 0) {
+    filterSideBar.setAttribute('style', 'left:0px !important');
+  }
+  flagFilter++;
+});
+
+filtercloseImg.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (flagFilter % 2 != 0) {
+    filterSideBar.setAttribute('style', 'left:-50vw !important');
+  }
+  flagFilter++;
+});
+// ====================================================================
+// chips items
+// ====================================================================
